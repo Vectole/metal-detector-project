3de974a847610c6eff58df35429b03f7286aafa6
Vectole <vectole@protonmail.com>
2019-08-21 03:52:24 +0300

    Removed display flicker, removed unused code, updated .gitignore

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 
 /// Microcontroller Datasheet:
 /// https://www.st.com/resource/en/datasheet/stm32f103c8.pdf
-
+extern crate panic_halt;
 #[macro_use()]
 use cortex_m_rt::entry;
 use embedded_hal::{digital::v2::OutputPin, PwmPin};
@@ -41,10 +41,6 @@ fn main() -> ! {
         gpiob.pb9.into_alternate_push_pull(&mut gpiob.crh),
     );
 
-    // let pwm_pins_2 = (
-    //     gpioa.pa8.into_alternate_push_pull(&mut gpioa.crh),
-    // );
-
     let (_, _, mut pwm_channel3, mut pwm_channel4) =
         device_peripherals
             .TIM4
@@ -56,16 +52,20 @@ fn main() -> ! {
     let mut voltmeter_controller = VoltmeterController {
         pin: &mut pwm_channel3,
     };
+
     let mut speaker_controller = SpeakerController {
         pin: &mut pwm_channel4,
     };
+
     // TODO: set pin PA8 as PWM
     // let mut bar_graph_controller = BarGraphController {
     //     pin: &mut pwm_channel3,
     // };
+
     let mut inductor_controller = InductorController {
         pin: &mut gpiob.pb1.into_push_pull_output(&mut gpiob.crl),
     };
+
     let mut display_controller = DisplayController {
         eight_segment_display_pin1: &mut gpioa.pa6.into_push_pull_output(&mut gpioa.crl),
         eight_segment_display_pin2: &mut gpioa.pa7.into_push_pull_output(&mut gpioa.crl),
@@ -73,17 +73,21 @@ fn main() -> ! {
         shift_register_data_pin: &mut gpioa.pa0.into_push_pull_output(&mut gpioa.crl),
         shift_register_data_clock_pin: &mut gpioa.pa3.into_push_pull_output(&mut gpioa.crl),
         shift_register_storage_clock_pin: &mut gpioa.pa2.into_push_pull_output(&mut gpioa.crl),
+        shift_register_enable_pin: &mut gpioa.pa1.into_push_pull_output(&mut gpioa.crl),
+        shift_register_clear_pin: &mut gpioa.pa4.into_push_pull_output(&mut gpioa.crl),
         current_display: 0,
         current_tick: 0,
         current_number: 0,
-        digit_bytes: &digit_bytes,
+        digit_bytes: &DIGIT_BYTES,
     };
+    display_controller.set_enabled(true);
+
     loop {
         inductor_controller.pulse_for(&mut delay_provider, 700);
         measurement_taker.read_after_waiting(&mut delay_provider, 40);
 
         voltmeter_controller.pulse(measurement_taker.output);
-        display_controller.display_number(&mut delay_provider, measurement_taker.output);
+        display_controller.display_number(measurement_taker.output);
         // speaker_controller.pulse(measurement_taker.output);
         // bar_graph_controller.pulse(measurement_taker.output);
         delay_provider.delay_us(6666 as u32);
@@ -104,7 +108,6 @@ struct VoltmeterController<'a> {
 
 impl VoltmeterController<'_> {
     fn pulse(&mut self, duty: u16) {
-        //self.pin.set_duty(duty);
         set_pwm(self.pin, duty, 800);
     }
 }
@@ -115,7 +118,6 @@ struct SpeakerController<'a> {
 
 impl SpeakerController<'_> {
     fn pulse(&mut self, duty: u16) {
-        //self.pin.set_duty(duty);
         set_pwm(self.pin, duty, 160);
     }
 }
@@ -126,7 +128,6 @@ struct BarGraphController<'a> {
 
 impl BarGraphController<'_> {
     fn pulse(&mut self, duty: u16) {
-        //self.pin.set_duty(duty);
         set_pwm(self.pin, duty, 280);
     }
 }
@@ -159,7 +160,7 @@ impl MeasurementTaker<'_> {
     }
 }
 
-const digit_bytes: [u8; 10] = [
+const DIGIT_BYTES: [u8; 10] = [
     0b10111110, //0
     0b10000010, //1
     0b11101100, //2
@@ -179,15 +180,27 @@ struct DisplayController<'a> {
     shift_register_data_pin: &'a mut dyn OutputPin<Error = Void>,
     shift_register_data_clock_pin: &'a mut dyn OutputPin<Error = Void>,
     shift_register_storage_clock_pin: &'a mut dyn OutputPin<Error = Void>,
+    shift_register_enable_pin: &'a mut dyn OutputPin<Error = Void>,
+    shift_register_clear_pin: &'a mut dyn OutputPin<Error = Void>,
     current_display: u8,
     current_tick: u8,
     current_number: u16,
     digit_bytes: &'static [u8; 10],
 }
 
-// TODO: figure out why it's flickering
 impl DisplayController<'_> {
-    fn display_number(&mut self, delay_provider: &mut Delay, input_value: u16) {
+    fn set_enabled(&mut self, enable: bool) {
+        // Active LOW (ŌĒ)
+        if enable {
+            self.shift_register_enable_pin.set_low();
+            self.shift_register_clear_pin.set_low();
+            self.shift_register_clear_pin.set_high();
+            self.shift_register_data_clock_pin.set_low();
+        } else {
+            self.shift_register_enable_pin.set_high();
+        }
+    }
+    fn display_number(&mut self, input_value: u16) {
         if self.current_tick == 0 {
             self.current_number = map(input_value, 0, 4095, 0, 999, false);
             self.current_tick = 50;
@@ -219,19 +232,16 @@ impl DisplayController<'_> {
             }
             _ => {}
         }
-        delay_provider.delay_us(1 as u8);
+        
         for i in 0..8 {
             digital_write(
                 self.shift_register_data_pin,
                 WriteState::Numeric(!self.digit_bytes[displayed_digit as usize] >> i & 0b0000_0001),
             );
             self.shift_register_data_clock_pin.set_high();
-            delay_provider.delay_us(1 as u8);
             self.shift_register_data_clock_pin.set_low();
         }
-        delay_provider.delay_us(1 as u8);
         self.shift_register_storage_clock_pin.set_high();
-        delay_provider.delay_us(1 as u8);
         self.shift_register_storage_clock_pin.set_low();
     }
 }
@@ -271,7 +281,7 @@ fn map(
     output_range_max: u16,
     reverse: bool,
 ) -> u16 {
-    let mut buffer: u32 = 0;
+    let buffer: u32;
     if value >= input_range_max {
         buffer = output_range_max as u32;
     } else if value <= input_range_min {
